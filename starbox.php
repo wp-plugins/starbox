075a686817a214b5b5afbc3c524b6ac3df34f905
cifi <cifi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-18 12:00:54 +0000

    Upgrade to 3.0.4
    git-svn-id: https://plugins.svn.wordpress.org/starbox/trunk@1248413 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- starbox.php
+++ starbox.php
@@ -8,11 +8,11 @@
   Plugin URI:
   Author: Squirrly UK
   Description: Starbox is the Author Box for Humans. Professional Themes to choose from, HTML5, Social Media Profiles, Google Authorship
-  Version: 3.0.3
+  Version: 3.0.4
   Author URI: https://www.squirrly.co/wordpress-seo-by-squirrly
  */
 /* SET THE CURRENT VERSION ABOVE AND BELOW */
-define('ABH_VERSION', '3.0.3');
+define('ABH_VERSION', '3.0.4');
 /* Call config files */
 require(dirname(__FILE__) . '/config/config.php');
 
